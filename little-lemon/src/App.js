1f6923150e5c58fc30fe3873fefafad0ab34081e
rkpal1207 <palrekha1995@gmail.com>
2023-05-31 12:23:04 -0700

    adding file

---
+++ little-lemon/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        header
+      </header>
+      <nav>
+        navbar
+      </nav>
+    </>
+  );
+}
+
+export default App;
