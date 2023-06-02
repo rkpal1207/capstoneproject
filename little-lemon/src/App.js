5f7ad4ef3b9b389deac9f00b9a511bd6fb2d6ea8
rkpal1207 <palrekha1995@gmail.com>
2023-06-01 23:24:31 -0700

    updated App.js file

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,17 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
-import Navbar from './Navbar';
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Navbar/>
-      <Main/>
-      <Footer/>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
